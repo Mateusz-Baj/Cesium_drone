c14a8b57e017f8c5a6c901100fe1ae2ef54c54af
Mateusz-Baj <mateusz.bajor11@gmail.com>
2025-06-10 16:24:10 +0200

    Update cesiumConfig.js

--- Cesium_Drone/cesiumConfig.js
+++ Cesium_Drone/cesiumConfig.js
@@ -1,5 +1,5 @@
 const accesToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmOWRlMjMxNS00NzJlLTQ0ZWItYTU5Zi05YzIyYmNmYjBlODIiLCJpZCI6MzEwNTQ0LCJpYXQiOjE3NDk0NzEwODJ9.eMPqpWXw-_UqE3Es23fKztd85a01Zvy92_Em4aF7wH0";
+  "Your acces Token";
   const uriPath = {
     aircraft: "./Cesium_Air.glb",
   };
